35d691fd0f48fdaf2d51b65d3f44a0b7d6674ed9
Stephen Jurgensen <sdjurgy@gmail.com>
2023-01-08 15:08:24 -0800

    update home.js

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import AddResaurant from '../components/AddResaurant';
 import Header from '../components/Header';
 import RestaurantList from '../components/RestaurantList';
+import RestaurantDetailPage from './RestaurantDetailPage';
+
 
 export const Home = () => {
     return (
@@ -9,6 +11,7 @@ export const Home = () => {
             <Header />
             <AddResaurant />
             <RestaurantList />
+            <RestaurantDetailPage />
         </div>
     );
 };
