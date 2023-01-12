f137cae6870d751e8953643e8db3e9a6051a1350
Stephen Jurgensen <sdjurgy@gmail.com>
2023-01-12 10:06:21 -0800

    update RestaurantFinder.js

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -6,12 +6,11 @@ import axios from "axios";
 
 
 // const baseURL = "http://localhost:3002/api/v1/restaurants";
-const baseURL = "api/v1/restaurants";
+// const baseURL = "api/v1/restaurants";
 
-// const baseURL = process.env.NODE_ENV === 'production' 
-//     ? "api/v1/restaurants" 
-//     : "https://localhost:3002/api/v1/restaurants";
-    // : "https://34.219.73.120/api/v1/restaurants";
+const baseURL = process.env.NODE_ENV === 'production' 
+    ? "api/v1/restaurants" 
+    : "https://localhost:3002/api/v1/restaurants";
 
 export default axios.create({
     baseURL,
