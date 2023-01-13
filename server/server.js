e2b7b5cdbd1b3799f4e09cbc059a38d9ade8de17
Stephen Jurgensen <sdjurgy@gmail.com>
2023-01-12 17:59:53 -0800

    update RestaurantFinder.js

--- server/server.js
+++ server/server.js
@@ -159,6 +159,25 @@ app.put("/api/v1/restaurants/:id", async (req, res) => {
         console.log(err);
     }
 });
+// Update a Restaurant 
+app.put("/restaurants/api/v1/restaurants/:id", async (req, res) => {
+    // console.log(req.body);
+    try {
+        const results = await db.query(
+            "UPDATE restaurants SET name = $1, location = $2, price_range = $3 where id = $4 returning *;",
+            [req.body.name, req.body.location, req.body.price_range, req.params.id],
+        );
+        // console.log(results);
+        res.status(200).json({
+            status: "success",
+            data: {
+                restaurant: results.rows[0],
+            }
+        });
+    } catch (err) {
+        console.log(err);
+    }
+});
 
 // Delete Restaurants
 app.delete("/api/v1/restaurants/:id", async (req, res) => {
